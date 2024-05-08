2365dcb7f7abdd88fc9d1377f4c71719d184742b
e.mi.zh <e.mi.zh@yandex.ru>
2024-05-08 18:33:36 +1000

    Added ZIP filenames print.

--- hw03/zipjpeg_test.c
+++ hw03/zipjpeg_test.c
@@ -2,21 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <stdbool.h>
+#include <stdint.h>
 
-#define JPEG_HEADER_LEN 2 
-#define ZIP_HEADER_LEN 4 
+#define JPEG_HEADER_LEN 2
+#define ZIP_HEADER_LEN 2
+#define ZIP_HEADER_TYPE_LEN 2
+#define MAX_FILENAME_LENGTH 256
+#define LOCAL_HEADER_SKIP_BYTES 22
+#define ZIP_FILENAME_LEN 2
+
+enum zip_record {
+	LOCAL_FILE_HEADER,
+	END_OF_CENTRAL_DIRECTORY
+};
+
+void print_usage(void);
+bool signature_search(FILE* f, unsigned char* signature, unsigned int length);
+int check_zip_header(FILE* f);
+int get_filename(FILE* f, unsigned char* fname);
 
 int main(int argc, char* argv[])
 {
 	FILE* f;
 	unsigned char jpeg_header[JPEG_HEADER_LEN] = { 0xFF, 0xD8 };
 	unsigned char jpeg_end[JPEG_HEADER_LEN] = { 0xFF, 0xD9 };
-	unsigned char buf[ZIP_HEADER_LEN];
-	unsigned char zip_header[ZIP_HEADER_LEN] = { 0x50, 0x4b, 0x03, 0x04 };
-	int res;
+	unsigned char fname[MAX_FILENAME_LENGTH];
+	unsigned char zip_header[ZIP_HEADER_LEN] = { 0x50, 0x4b };
 
 	if (argc < 2) {
 		fprintf(stderr, "Input file not specified!\n");
+		print_usage();
 		exit(EXIT_FAILURE);
 	}
 
@@ -27,33 +43,112 @@ int main(int argc, char* argv[])
 	}
 	printf("Opened file %s, starting analyze...\n", argv[1]);
 
-	res = fread(buf, 1, JPEG_HEADER_LEN, f);
-	if (res < JPEG_HEADER_LEN) {
-		fprintf(stderr, "File %s too short, read only %d bytes! Exiting...\n", argv[1], res);
-		fclose(f);
-		exit(EXIT_FAILURE);
-	}
-
-	res = memcmp(buf, jpeg_header, JPEG_HEADER_LEN);
-	if (res == 0) {
+	if (signature_search(f, jpeg_header, JPEG_HEADER_LEN)) {
 		printf("JPEG signature detected! Trying to reach end...\n");
-		do {
-			buf[1] = buf[0];
-			buf[0] = (unsigned char) fgetc(f);
-#ifdef DEBUG
-			printf("buf=%x%x\tpos=%ld\tfeof(f)=%d\tres=%d\n", buf[0], buf[1], ftell(f), feof(f), memcmp(buf, jpeg_end, JPEG_HEADER_LEN));
-#endif
-		} while ((res = memcmp(buf, jpeg_end, JPEG_HEADER_LEN) != 0) && !feof(f));
-		if (res != 0) {
+		if (signature_search(f, jpeg_end, JPEG_HEADER_LEN)) {
+			printf("JPEG end signature detected!\n");
+		} else {
 			printf("JPEG file end not detected! Possibly broken image, exiting...\n");
 			fclose(f);
 			exit(EXIT_FAILURE);
-		} else {
-			printf("JPEG end signature detected!\n");
 		}
-	printf("Begin archive signature search.\n");
+	} else {
+		printf("Hmm, JPEG header not found, maybe simple archive?..\n");
+		rewind(f);
 	}
 
+	printf("Begin archive signature search.\n");
+	while ((!feof(f)) && (signature_search(f, zip_header, ZIP_HEADER_LEN))) {
+		switch (check_zip_header(f)) {
+			case LOCAL_FILE_HEADER:
+				if (!get_filename(f, fname)) {
+					printf("Found ZIP with file %s\n", fname);
+				}
+				break
+			case END_OF_CENTRAL_DIRECTORY:
+				printf("ZIP file end (EOCD  record) found.\n");
+				fclose(f);
+				exit(EXIT_SUCCESS);
+			case -1:
+				break;
+			default:
+				break;
+		}
+	}
 
+	fclose(f);
 	exit(EXIT_SUCCESS);
+}
+
+bool signature_search(FILE* f, unsigned char signature[], unsigned int length) {
+	unsigned int pos = 0;
+	bool found = false;
+	int c;
+
+	while ((!found) && ((c = fgetc(f)) != EOF)) {
+		if (signature[pos] == (unsigned char) c) {
+			pos++;
+		} else {
+			pos = 0;
+		}
+		if (pos == length) {
+			found = true;
+		}
+	}
+	if (feof(f)) {
+		fprintf(stderr, "End of file reached.\n");
+	}
+
+	return(found);
+}
+
+int check_zip_header(FILE* f) {
+	unsigned char buf[ZIP_HEADER_TYPE_LEN];
+	unsigned char zip_file_header[ZIP_HEADER_LEN] = { 0x03, 0x04 };
+	unsigned char zip_eocd_record[ZIP_HEADER_LEN] = { 0x05, 0x06 };
+
+	if (fread(buf, 1, ZIP_HEADER_TYPE_LEN, f) < ZIP_HEADER_TYPE_LEN) {
+		fprintf(stderr, "End of file reached.\n");
+		return -1;
+	}
+	if (!memcmp(buf, zip_file_header, ZIP_HEADER_TYPE_LEN)) {
+		return LOCAL_FILE_HEADER;
+	} else {
+		if (!memcmp(buf, zip_eocd_record, ZIP_HEADER_TYPE_LEN)) {
+			return END_OF_CENTRAL_DIRECTORY;
+		}
+	}
+
+	fseek(f, -ZIP_HEADER_TYPE_LEN, SEEK_CUR);
+	return -1;
+}
+
+void print_usage(void) {
+	printf("Usage: zipjpeg_test filename\n");
+}
+
+int get_filename(FILE* f, unsigned char* fname) {
+	uint16_t size;
+	int res;
+
+	fseek(f, LOCAL_HEADER_SKIP_BYTES, SEEK_CUR);
+	res = fread(&size, 1, ZIP_FILENAME_LEN, f);
+	if (res < ZIP_FILENAME_LEN) {
+		fprintf(stderr, "Failed to read zip filename size: wanted %d, get only %d!\n", ZIP_FILENAME_LEN, res);
+		return -1;
+	}
+	if (size >= MAX_FILENAME_LENGTH) {
+		size = MAX_FILENAME_LENGTH - 1;
+	}
+	if (fseek(f, 2, SEEK_CUR)) {
+		fprintf(stderr, "Error seeking in file: %s.\n", strerror(errno));
+		return(-1);
+	}
+	res = fread(fname, 1, size, f);
+	if (res < size) {
+		fprintf(stderr, "Failed to read zip filename: wanted %d, get only %d!\n", size, res);
+		return -1;
+	}
+	fname[size] = '\0';
+	return 0;
 }
