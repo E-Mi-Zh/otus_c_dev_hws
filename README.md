f120eac1c274c2b88492478565a3a8292a1c50ed
e.mi.zh <e.mi.zh@yandex.ru>
2024-08-21 15:52:24 +1000

    hw09: ДЗ по основам ассемблера.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 * [hw\_04](https://github.com/E-Mi-Zh/otus_c_dev_hws/tree/master/hw04) - перекодировка из 8-битной кодировки в UTF-8
 * [hw\_06](https://github.com/E-Mi-Zh/otus_c_dev_hws/tree/master/hw06) - подсчёт частоты встречаемости слов с использованием хеш-таблицы
 * [hw\_08](https://github.com/E-Mi-Zh/otus_c_dev_hws/tree/master/hw08) - получение прогноза погоды с wttr.in (libcurl, парсинг JSON)
+* [hw\_09](https://github.com/E-Mi-Zh/otus_c_dev_hws/tree/master/hw09) - основы ассемблера
 * [hw\_12](https://github.com/E-Mi-Zh/otus_c_dev_hws/tree/master/hw12) - сборка Curl с поддержкой заданных протоколов
 * [hw\_13](https://github.com/E-Mi-Zh/otus_c_dev_hws/tree/master/hw13) - поиск и исправление утечек памяти с помощью Valgrind
 * [hw\_15](https://github.com/E-Mi-Zh/otus_c_dev_hws/tree/master/hw15) - рекурсивное отображение списка файлов в GTK4
---
+++ hw09/c-prog.c
@@ -0,0 +1,113 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+long data[] = {4, 8, 15, 16, 23, 42};
+size_t data_length = sizeof(data) / sizeof(data[0]);
+char* int_format = "%ld ";
+char* empty_str = "";
+
+typedef struct node {
+    long val;
+    struct node* next;
+} node_t;
+
+void print_int(long number)
+{
+	printf(int_format, number);
+	fflush(NULL);
+}
+
+long p(long number)
+{
+	return(number & 1);
+}
+
+node_t* add_element(long val, node_t* head)
+{
+	node_t* new_node;
+
+	new_node = (node_t *) malloc(sizeof(node_t));
+	if (new_node == NULL) {
+        abort();
+    }
+
+    new_node->val = val;
+    new_node->next = head;
+
+    return new_node;
+}
+
+/* map - функция высшего порядка */
+/* принимает аргументом список и функцию, которую нужно применить */
+/* к каждому элементу списка */
+void m(node_t* list, void (*function)(long))
+{
+	if (list == NULL) return;
+	function(list->val);
+	m(list->next, function);
+}
+
+/* Пытался сделать вариант с возвратом через результат функции */
+/* Но в этом случае итоговый список оказывался с инверсным порядком */
+/* Что не совпадало с выводом программы на ассемблере */
+/*node_t* f(node_t* list, long (*function)(long))
+{
+    node_t* l;
+	if (list == NULL) return NULL;
+
+	if (function(list->val)) {
+		return add_element(list->val, f(list->next, function));
+	} else {
+        return f(list->next, function);
+    }
+}*/
+
+/* filter - функция высшего порядка */
+/* принимает аргументом список и функцию, которую нужно применить */
+/* к каждому элементу списка */
+/* в качестве результата возвращает новый список, состоящий из тех членов исходного, */
+/* для которых функция-аргумент дала true */
+void f(node_t* list, node_t** result, long (*function)(long))
+{
+	if (list == NULL) return;
+
+	if (function(list->val)) {
+		*result = add_element(list->val, *result);
+	}
+	f(list->next, result, function);
+}
+
+void free_list(node_t* list)
+{
+	node_t* tmp;
+
+	while (list->next != NULL) {
+		tmp = list;
+		list = list->next;
+		free(tmp);
+    }
+}
+
+int main(void)
+{
+	node_t* numbers_list = NULL;
+	node_t* list2 = NULL;
+
+	for (size_t i = data_length; i != 0; --i) {
+	        numbers_list = add_element(data[i-1], numbers_list);
+	}
+
+	m(numbers_list, &print_int);
+	puts(empty_str);
+
+	f(numbers_list, &list2, &p);
+
+	m(list2, &print_int);
+	puts(empty_str);
+
+	/* здесь была утечка, т.к. в исходном листинге списки не освобождались */
+//	free_list(numbers_list);
+	free_list(list2);
+
+	return 0;
+}
